973dcbe7cbb18dd553752c2be1f5743d3fb560f8
Brandon Claypool <claypoob3195@student.faytechcc.edu>
2025-09-07 20:59:50 +0000

    Finished M1T1

--- m1t1_0901.cpp
+++ m1t1_0901.cpp
@@ -6,6 +6,19 @@ claypoob
 Program will greet the user.
 */
 
+//magic words
+#include <iostream>
+using namespace std;
 
+int main() {
+    // input: start the program
+    // processing: none
+    // output: 0 (no errors)
+    // special FX: Says "Hello" to the user.
+
+    // Let's say hello
+    cout << "Hello, user!" << endl; // "end line" not "end 1"
+    return 0; // no errors
+}
 
 
