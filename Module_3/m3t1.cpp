867d97f8fa1bf6280e356096175315b493956553
Brandon Claypool <claypoob3195@student.faytechcc.edu>
2025-10-12 20:12:50 +0000

    updated m3t1

--- Module_3/m3t1.cpp
+++ Module_3/m3t1.cpp
@@ -1,7 +1,7 @@
 /* CSC 134
 M3T1 - 
 claypoolb
-10/05/25
+10/12/25
 Program to 
 */
 
@@ -13,21 +13,33 @@ int main() {
 
     // Variables
     double len1, wid1, len2, wid2; 
-    double area1, area2;
 
     // Ask user for the length and width
     cout << "What's the length of the first rectangle? ";
     cin >> len1;
-    cout << "What's the width of the first rectangle? ";
+    cout << "\nWhat's the width of the first rectangle? ";
     cin >> wid1;
-    cout << "What's the length of the second rectangle? ";
+    cout << "\nWhat's the length of the second rectangle? ";
     cin >> len2;
-    cout << "What's the width of the second rectangle? ";
+    cout << "\nWhat's the width of the second rectangle? ";
     cin >> wid2;
 
     // Find the area
+    double area1 = len1 * wid1;
+    double area2 = len2 * wid2;
 
     // Print the area
+    cout << "\n\nArea of rectangle 1: " << area1 << endl;
+    cout << "\nArea of rectangle 2: " << area2 << endl;
+
+    // Determine which is bigger
+    if (area1 > area2) {
+        cout << "\n\nRectangle 1 has the larger area.\n\n\n";
+    } else if (area2 > area1) {
+        cout << "\n\nRectangle 2 has the larger area.\n\n\n";
+    } else if (area1 == area2) {
+        cout << "\n\nBoth rectangles have the same area.\n\n\n";
+    }
 
     return 0;
 }
