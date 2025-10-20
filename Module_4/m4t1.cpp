89b0e771f2404b859effa68633ff4a856856d3e0
Brandon Claypool <claypoob3195@student.faytechcc.edu>
2025-10-20 03:30:33 +0000

    started m4t1

---
+++ Module_4/m4t1.cpp
@@ -0,0 +1,32 @@
+
+/* CSC 134
+M4T1 - 
+claypoolb
+10/19/25
+While loop
+*/
+
+
+#include <iostream>
+using namespace std;
+  
+int main() {
+    // program 5-3
+    // Using a simple while loop
+
+    // Part 1, just say "hi" five times
+    int number = 5;
+
+    int i = 0;
+    while (i < number) {
+        cout << "Hello" << endl;
+        //i = i + 1
+        i++;
+    }
+    cout << "That's all!" << endl;
+
+    // program 5-6
+
+ 
+    return 0; // done
+}
